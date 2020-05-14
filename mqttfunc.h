0d53dedefe36afc40cffa860ebbea9bf3cae2507
Amir <amirz98@hotmail.com>
2020-05-14 21:25:32 +0800

    Expose reboot function

--- mqttfunc.h
+++ mqttfunc.h
@@ -16,6 +16,7 @@ void subscribemqtt()
   client.subscribe("/myroom/relay/1");
   client.subscribe("/myroom/relay/2");
   client.subscribe("/myroom/relay/3");
+  client.subscribe("/node_relay/reboot");
 }
 
 void reconnect() {
