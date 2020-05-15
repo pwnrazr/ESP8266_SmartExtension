b1089f1abebed00084a65f83de3e4563eece11e0
Amir <amirz98@hotmail.com>
2020-05-15 22:48:54 +0800

    MQTT: some cleanup of unused things

--- ESP8266_Room-Lights-Control.ino
+++ ESP8266_Room-Lights-Control.ino
@@ -189,51 +189,25 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
 }
 
 void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
 }
 
 void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
 }
 
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
   Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
+  Serial.println(topic);;
   
-  Serial.println(total);
-
   String payloadstr;
 
   for (int i = 0; i < len; i++) 
   {
     payloadstr = String(payloadstr + (char)payload[i]);  //convert payload to string
   }
-
   Serial.println(payloadstr);
 }
 
 void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
 }
 
 void commssetup() {
